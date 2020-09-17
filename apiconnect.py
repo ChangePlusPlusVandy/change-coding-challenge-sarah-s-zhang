d8477ed370225ffa89d5775f8b63fa586f0a0f60
Sarah Zhang <sarah.s.zhang@vanderbilt.edu>
2020-09-17 01:14:05 -0500

    First redesign

---
+++ apiconnect.py
@@ -0,0 +1,30 @@
+import config
+import requests
+
+class ApiConnect:
+
+    def __init__(self, username):
+        self.username = username
+
+    def __create_headers(self, bearer_token):
+        headers = {"Authorization": "Bearer {}".format(bearer_token)}
+        return headers
+
+    def __create_url(self, username):
+        numTweets = 5
+        url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={}&count={}&exclude_replies=true".format(
+            username, numTweets
+        )
+        return url
+    
+    def __connect_to_endpoint(self, url, headers):
+        response = requests.request("GET", url, headers=headers)
+        if response.status_code != 200:
+            raise Exception(response.status_code, response.text)
+        return response.json()
+    
+    def getTweets(self):
+        headers = self.__create_headers(config.bearer_token)
+        url = self.__create_url(self.username)
+        jsonTweets = self.__connect_to_endpoint(url, headers)
+        return jsonTweets
--- main.py
+++ main.py
@@ -1,73 +1,44 @@
-import requests
 import json
 import random
+from apiconnect import ApiConnect
 
-# To set your enviornment variables in your terminal run the following line:
-# export 'BEARER_TOKEN'='<your_bearer_token>'
-
-
-def auth():
-    return "AAAAAAAAAAAAAAAAAAAAAPX4HgEAAAAAf34E7izTYWADOluGxYpHB4ToKU0%3DvNiWMhi2rToyZJSxjDH2kJKrEzcMTX20ePY3mQdzyHh8gYQKXm"
 
 def welcome():
     print("Hello! Welcome to the Tweet Guessing Game.")
-    print("Given a tweet by one of two individuals, guess who wrote the tweet!")
-
-
-def create_url(username):
-    numTweets = 2
-    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={}&count={}&exclude_replies=true".format(
-        username, numTweets
-    )
-    return url
-
-
-def create_headers(bearer_token):
-    headers = {"Authorization": "Bearer {}".format(bearer_token)}
-    return headers
-
-
-def connect_to_endpoint(url, headers):
-    response = requests.request("GET", url, headers=headers)
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    return response.json()
-
+    print("Given a tweet by one of two individuals, guess who wrote the tweet!\n")
 
 def randomUserGenerator():
     return random.randint(1, 2)
 
 def randomTweetGenerator(userNumber, userOneTweets, userTwoTweets):
     if userNumber == 1:
-        tweetIndex = round(random.random()*len(userOneTweets))
-        return userOneTweets[int(tweetIndex)]
+        return random.choice(userOneTweets)
     else:
-        tweetIndex = round(random.random()*len(userTwoTweets))
-        return userTwoTweets[int(tweetIndex)]
+        return random.choice(userTwoTweets)
 
 
 def verifyGuess(userNumber, guess):
     if userNumber == guess:
-        print("Correct!\n")
+        print("\nCorrect!\n")
+        return True
     else:
-        print("Incorrect!\n")
+        print("\nIncorrect!\n")
+        return False
 
 
 def main():
     welcome()
-    bearer_token = auth()
-    headers = create_headers(bearer_token)
+    
+    userOne = ApiConnect("elonmusk")
+    json_tweets_1 = userOne.getTweets()
 
-    url = create_url("elonmusk")
-    json_tweets_1 = connect_to_endpoint(url, headers)
-
-    url = create_url("kanyewest")
-    json_tweets_2 = connect_to_endpoint(url, headers)
+    userTwo = ApiConnect("kanyewest")
+    json_tweets_2 = userTwo.getTweets()
 
     userNumber = randomUserGenerator()
     jsonTweet = randomTweetGenerator(userNumber, json_tweets_1, json_tweets_2)
     print(jsonTweet['text'])
-    guess = input("Who wrote this tweet? Enter '1' for Elon Musk, '2' for Kanye West:")
+    guess = input("\nWho wrote this tweet? Enter '1' for Elon Musk, '2' for Kanye West: ")
     verifyGuess(userNumber, int(guess))
 
 
